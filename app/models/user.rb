f138a998da644d710bb695349b09f76a3dbc6b73
TASUKE <tasuke_0821@yahoo.co.jp>
2017-07-26 13:37:36 +0000

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
+  
+  
 end
